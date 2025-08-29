d0d5b6ffae335fd1d172232dbc30d52b7b0ac3ce
Edison Andres M. <132613148+AndresM92@users.noreply.github.com>
2025-08-28 21:34:01 -0500

    Created new models
    DataPayment and URLPaypalResponse

---
+++ backend/src/main/java/com/andres/ecommerce/backend/domain/model/DataPayment.java
@@ -0,0 +1,11 @@
+package com.andres.ecommerce.backend.domain.model;
+
+import lombok.Data;
+
+@Data
+public class DataPayment {
+    private String method;
+    private String amount;
+    private String currency;
+    private String description;
+}
---
+++ backend/src/main/java/com/andres/ecommerce/backend/domain/model/URLPaypalResponse.java
@@ -0,0 +1,10 @@
+package com.andres.ecommerce.backend.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class URLPaypalResponse {
+    private String url;
+}
