d441c931c6fdbf5c939acc55382b8628d087e8d6
Edison Andres M. <132613148+AndresM92@users.noreply.github.com>
2025-08-31 21:46:05 -0500

    modified services

---
+++ front_Ecommerce/src/app/common/user.ts
@@ -0,0 +1,13 @@
+export class User {
+
+    id!:number;
+    username!:String;
+    firstName!:String;
+    lastName!:String;
+    email!:String;
+    address!:String;
+    cellphone!:String;
+    password!:String;
+    userType!:String
+    
+}
